5cabf2eab2b549b7204aa06f9a67c61d42fdfa7a
Luiz Henrique Lajús <95000306+LuizHenriqueLajus@users.noreply.github.com>
2022-07-18 21:19:14 -0300

    Basic character movements
    Get and Set, Capsule Collider 2d, Rigidbody 2D, SCRIPTS to use Horizontal movements.

---
+++ Castlevania Project/Assets/GameFolder/Player/Script/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Vector2 vel; //holds velocity set numbers.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vel = new Vector2(Input.GetAxisRaw("Horizontal"), rb.velocity.y);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = vel;
+    }
+}
